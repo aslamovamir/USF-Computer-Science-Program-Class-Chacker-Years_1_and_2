0a8885318ef4466007d7846df8987fec78cdb528
Amir Aslamov <amiraslamov@usf.edu>
2022-02-14 02:33:10 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Class_TX = new System.Windows.Forms.TextBox();
+            this.Submit_BTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(223, 161);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(341, 217);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label5.Font = new System.Drawing.Font("Adobe Hebrew", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(321, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(154, 29);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Enter the class:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Font = new System.Drawing.Font("Adobe Hebrew", 20.25F);
+            this.label6.Location = new System.Drawing.Point(232, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(314, 36);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Welcome to Class Checker";
+            // 
+            // Class_TX
+            // 
+            this.Class_TX.Location = new System.Drawing.Point(326, 269);
+            this.Class_TX.Name = "Class_TX";
+            this.Class_TX.Size = new System.Drawing.Size(137, 20);
+            this.Class_TX.TabIndex = 7;
+            // 
+            // Submit_BTN
+            // 
+            this.Submit_BTN.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Submit_BTN.Font = new System.Drawing.Font("Adobe Hebrew", 11.25F);
+            this.Submit_BTN.Location = new System.Drawing.Point(358, 313);
+            this.Submit_BTN.Name = "Submit_BTN";
+            this.Submit_BTN.Size = new System.Drawing.Size(75, 28);
+            this.Submit_BTN.TabIndex = 8;
+            this.Submit_BTN.Text = "Submit";
+            this.Submit_BTN.UseVisualStyleBackColor = false;
+            this.Submit_BTN.Click += new System.EventHandler(this.Submit_BTN_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Adobe Hebrew", 16.25F);
+            this.label1.Location = new System.Drawing.Point(161, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(478, 30);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "USF Computer Science Program: Year 1 and Year 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(323, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "*enter course label and then code";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Submit_BTN);
+            this.Controls.Add(this.Class_TX);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Class_TX;
+        private System.Windows.Forms.Button Submit_BTN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //helper function to display all the classes the user can take after a given class
+        public bool check_after(string user_input)
+        {
+            //this variable will help to determine if the determination of the next classes has been successful
+            bool success = true;
+            //we will user switch block to determine the classes
+            switch (user_input)
+            {
+                //first level
+                case "MAC2281":
+                    MessageBox.Show("You can take: PHY 2048, MAC 2282 or MAC 2312, COP 2510, COT 3100");
+                    break;
+                case "MAC2311":
+                    MessageBox.Show("You can take: PHY 2048, MAC 2282 or MAC 2312, COP 2510, COT 3100");
+                    break;
+
+                //second level
+                case "PHY2048":
+                    MessageBox.Show("You can take: PHY 2049");
+                    break;
+                case "MAC2282":
+                    MessageBox.Show("You can take: PHY 2049, MAC 2283 or MAC 2313");
+                    break;
+                case "MAC2312":
+                    MessageBox.Show("You can take: PHY 2049, MAC 2283 or MAC 2313");
+                    break;
+                case "COP2510":
+                    MessageBox.Show("You can take: CDA 3103, COP 3514");
+                    break;
+                case "COT3100":
+                    MessageBox.Show("Error! You have to be in Upper Division to take a class after COT 3100, contact your advisor.");
+                    break;
+
+                //third level
+                case "PHY2049":
+                    MessageBox.Show("You do not have any other class to take after PHY 2049.");
+                    break;
+                case "MAC2283":
+                    MessageBox.Show("You do not have any other class to take after MAC 2283.");
+                    break;
+                case "MAC2313":
+                    MessageBox.Show("You do not have any other class to take after MAC 2313");
+                    break;
+                case "CDA3103":
+                    MessageBox.Show("You can take: CDA 3201, COP 4530");
+                    break;
+                case "COP3514":
+                    MessageBox.Show("You can take: CDA 3201, COP 4530");
+                    break;
+
+                //fourth level
+                case "CDA3201":
+                    MessageBox.Show("Error! You have to be in Upper Division to take a class after CDA 3201, contact your advisor");
+                    break;
+                case "COP4530":
+                    MessageBox.Show("Error! You have to be in Upper Division to take a class after COP 4530, contact your advisor");
+                    break;
+
+                //invalid input
+                default:
+                    MessageBox.Show("Error! You have entered invalid input! Try again!");
+                    success = false;
+                    //reset the user input textbox
+                    Class_TX.Text = "";
+                    break;
+            }
+
+            return success;
+        }
+
+
+        //helper function to determine all the classes the user must have taken based on the class enterred
+        public void check_before(string user_input)
+        {
+            //we will use if statements to deterime the classes the user must have taken
+            
+            //first level
+            if (user_input == "MAC2281" || user_input == "MAC2311")
+            {
+                MessageBox.Show("You do not have any classes you have taken before MAC 2281 or MAC 2311");
+            }
+
+            //second level
+            else if (user_input == "PHY2048" || user_input == "MAC2282" || user_input == "MAC2312" ||
+                user_input == "COP2510" || user_input == "COT3100")
+            {
+                MessageBox.Show("You must have taken before: MAC 2281 or MAC 2311");
+            }
+    
+            //third level
+            else if (user_input == "PHY2049")
+            {
+                MessageBox.Show("You must have taken before: PHY 2048, MAC 2282 or MAC 2312, MAC 2281 or MAC 2311");
+            }
+            else if (user_input == "MAC2283" || user_input == "MAC2313")
+            {
+                MessageBox.Show("You must have taken before: MAC 2282 or MAC 2312, MAC 2281 or MAC 2311");
+            }
+            else if (user_input == "CDA3103")
+            {
+                MessageBox.Show("You must have taken before: COP 2510, MAC 2281 or MAC 2311");
+            }
+            else if (user_input == "COP3514")
+            {
+                MessageBox.Show("You must have taken before: COP 2510, MAC 2281, MAC 2311"); 
+            }
+
+            //fourth level
+            else if (user_input == "CDA3201" || user_input == "COP4530")
+            {
+                MessageBox.Show("You must have taken before: CDA 3103, COP 3514, COP 2510, MAC 2281 or MAC 2311");
+            }
+
+            //if none of the if statements is true, we know the user input is invalid
+            else
+            {
+                MessageBox.Show("Error! Invalid Input!");
+            }
+
+            //reset the input text box
+            Class_TX.Text = "";
+        }
+
+
+        private void Submit_BTN_Click(object sender, EventArgs e)
+        {
+            //get the user input
+            //we don't want the user to submit an empty input
+            if (Class_TX.Text == "")
+            {
+                //let the user know about the empty input error
+                MessageBox.Show("Error! Input cannot be empty!");
+                return;
+            }
+            string user_input = Class_TX.Text;
+
+            //let's first check the classes the user can take after the class entered
+            //we will call the helper function to do this
+            //if the next classes determination has been successful we proceed to check 
+            //the classes the user must have already taken
+            if (check_after(user_input))
+            {
+                check_before(user_input);
+            }
+        }
+    }
+}
